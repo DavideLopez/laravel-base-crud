a987f86d0d2683add96ef925afbc20340e9f4f19
DavideLopez <davidelopez7@gmail.com>
2022-10-15 20:23:08 +0200

    add index + fine

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -11,6 +12,21 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics_data');
+
+        foreach ($comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+
+            $newComic->save();
+        }
+
     }
 }
